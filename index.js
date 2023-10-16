52bc10e98d1f6ca7b3ad6a13b689011601cad41f
MarleneElla <ellamarlene54@gmail.com>
2023-10-16 09:49:53 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Write your solution in this file!
+// Define the employee object
+let employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway"
+  };
+  
+  // Function to update an employee with a new key-value pair (non-destructive)
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+  
+  // Function to update an employee with a new key-value pair (destructive)
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  // Function to delete a key from an employee (non-destructive)
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  // Function to delete a key from an employee (destructive)
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+  // Export the functions
+  module.exports = {
+    updateEmployeeWithKeyAndValue,
+    destructivelyUpdateEmployeeWithKeyAndValue,
+    deleteFromEmployeeByKey,
+    destructivelyDeleteFromEmployeeByKey,
+  };
+  
+  // Example usage of the functions
+  let newEmployee = updateEmployeeWithKeyAndValue(employee, "name", "Jane Smith");
+  console.log(newEmployee);
+  
+  let mutatedEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee, "streetAddress", "456 Elm St");
+  console.log(mutatedEmployee);
+  
+  let deletedEmployee = deleteFromEmployeeByKey(employee, "name");
+  console.log(deletedEmployee);
+  
+  let deletedMutatedEmployee = destructivelyDeleteFromEmployeeByKey(employee, "streetAddress");
+  console.log(deletedMutatedEmployee);
+  
